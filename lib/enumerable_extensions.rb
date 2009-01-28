b34bb860b086011084293671abc93f2d118d4f7c
Semin Lee <seminlee@gmail.com>
2009-01-28 12:55:10 +0000

    lib/enumerable_extensions.rb deleted

--- lib/enumerable_extensions.rb
+++
@@ -1,7 +0,0 @@
-module EnumerableExtensions
-  def cart_prod( *args )
-    args.inject([[]]) { |old, lst| new = []; lst.each { |e| new += old.map { |c| c.dup << e } } ; new }
-  end
-end
-
-Enumerable.extend(EnumerableExtensions)
